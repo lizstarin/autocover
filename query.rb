927be80f6fd26391df96db56b1abd37646732bdb
Liz Starin <liz.starin@gmail.com>
2015-01-14 17:34:42 -0500

    playing with search params

--- query.rb
+++ query.rb
@@ -5,7 +5,7 @@ require 'cgi'
 require 'erb'
 
 def query_with_terms(search_terms)
-	url = "http://api.repo.nypl.org/api/v1/items/search.json?q=#{search_terms}"
+	url = "http://api.repo.nypl.org/api/v1/items/search.json?per_page=100&q=#{search_terms}"
 	uri = URI.parse(url)
 	http = Net::HTTP.new(uri.host, uri.port)
 
@@ -18,24 +18,31 @@ def query_with_terms(search_terms)
 	JSON.parse(response.body)['nyplAPI']['response']['result']
 end
 
-books = []
-open('ebookhack/30_metadata.json').each do |line|
-	metadata = JSON.parse(line)
+def build_book(metadata)
 	subjects = []
 
 	title = metadata['title']
 	author = metadata['authors'][0] ? metadata['authors'][0]['display_name'] : nil
 
-	book = {'title' => title, 'author' => author}
-
 	metadata['subjects'].each do |s|
 		subjects << CGI::escape(s['identifier'].split(' -- ')[0]) if s['type'] == 'LCSH'
 	end
+
+	book = {'title' => title, 'author' => author, 'subjects' => subjects}
+end
+
+
+books = []
+open('ebookhack/30_metadata.json').each do |line|
+	metadata = JSON.parse(line)
+	book = build_book(metadata)
 	
+	subjects = book['subjects']
 	search_terms = subjects.join("%20")
+	# search_terms = subjects.last
 	response = query_with_terms(search_terms)
 
-	book['image_id'] = response.first['imageID']
+	book['image_id'] = response.sample['imageID']
 	books << book
 end
 
